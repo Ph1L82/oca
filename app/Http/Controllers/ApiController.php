b8c216475d04af68155e6ca290d7ff4a7bda5abb
Ph1L82 <eduardo.rodriguez.bahamonde@gmail.com>
2017-07-01 21:51:57 -0400

    removes env from paginate method

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -137,7 +137,7 @@ trait ApiController {
         ]);
     }
 
-    public function paginate($items, $perPage = env('PAGINATE_SIZE'))
+    public function paginate($items, $perPage)
     {
         $pageStart = \Request::get('page', 1);
         $offSet = ($pageStart * $perPage) - $perPage;
